96805da5f0522f9c08b4a1a33db564b163636827
Tommaso Patriti <tommaso.patriti@studio.unibo.it>
2023-10-10 10:46:35 +0200

    py view controller

---
+++ python/LightControlloer.py
@@ -0,0 +1,67 @@
+import tkinter as tk
+from enum import Enum
+root = tk.Tk()
+tk.Entry(root, justify='center')
+root.geometry("400x300")
+root.title("Light Controller")
+
+parameter = Enum('Frequence', ['BASS', 'MEDIUM', 'STATUS'])
+is_on = False
+
+
+def bass_slider_changed(value):
+    send_to_serial(value, parameter.BASS)
+
+def medium_slider_changed(value):
+    send_to_serial(value, parameter.MEDIUM)
+
+def light_status():
+    global is_on
+    send_to_serial(is_on, parameter.STATUS)
+
+
+bass_slider = tk.Scale(root, from_=0, to=100, orient=tk.VERTICAL, command=bass_slider_changed)
+bass_slider.grid(row=0, column=0)
+tk.Label(root, text="Bass").grid(row=1, column=0)
+
+
+medium_slider = tk.Scale(root, from_=0, to=100, orient=tk.VERTICAL, command=medium_slider_changed)
+medium_slider.grid(row=0, column=1)
+tk.Label(root, text="Medium").grid(row=1, column=1)
+
+
+
+
+def on_off_switch_event():
+    global is_on
+    if is_on:
+        is_on = False
+        toggle_on__off_switch.config(text="OFF")
+    else:
+        is_on = True
+        toggle_on__off_switch.config(text="ON")
+    light_status()
+
+
+toggle_on__off_switch = tk.Button(text="OFF", width=10, command=on_off_switch_event)
+toggle_on__off_switch.grid(row=2, column=0, columnspan=2)
+
+
+
+
+
+
+
+def send_to_serial(value, parameter):
+    if parameter == parameter.BASS:
+        print("Bass", value)
+    elif parameter == parameter.MEDIUM:
+        print("Medium", value)
+    elif parameter == parameter.STATUS:
+        print("Status", value)
+
+
+
+
+
+root.mainloop()
